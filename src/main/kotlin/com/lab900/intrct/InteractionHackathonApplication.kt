821610ebd47b61fc5f532fb923a716b162a7dc6c
Luis Santos <luis.santos@lab900.com>
2022-06-08 15:00:35 +0200

    feat: spring boot files

---
+++ src/main/kotlin/com/lab900/intrct/InteractionHackathonApplication.kt
@@ -0,0 +1,11 @@
+package com.lab900.intrct
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InteractionHackathonApplication
+
+fun main(args: Array<String>) {
+	runApplication<InteractionHackathonApplication>(*args)
+}
---
+++ src/test/kotlin/com/lab900/intrct/InteractionHackathonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lab900.intrct
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InteractionHackathonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
