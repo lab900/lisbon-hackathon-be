d47aff19f546bdf2f39f4a40affabb03e2242b34
lab900-sander-van-loock <sander.vanloock@lab900.com>
2022-06-09 20:21:24 +0200

    controller

---
+++ src/main/kotlin/com/lab900/intrct/domain/controller/TimelineConfigController.java
@@ -0,0 +1,29 @@
+package com.lab900.intrct.domain.controller;
+
+import com.lab900.intrct.domain.TimelineConfig;
+import com.lab900.intrct.domain.repository.TimelineConfigJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class TimelineConfigController {
+
+    private final TimelineConfigJpaRepository repo;
+
+    @GetMapping("/v1/config")
+    public List<TimelineConfig> all() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/v1/config/${id}")
+    public TimelineConfig one(@PathVariable Long id) {
+        return repo.findById(id).orElseThrow();
+    }
+
+
+}
